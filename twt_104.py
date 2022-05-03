5225a8f7ce8512c362abddb4ab61074d3c1b26cd
RDornodon <archelfhun@gmail.com>
2022-05-03 17:41:00 +0200

    Submitted version

--- twt_104.py
+++ twt_104.py
@@ -1,5 +1,1 @@
-_,*A=open(0)
-while A:
- _,a,*A=A;r=b=0;a=*map(int,a.split()),
- for v in a:b+=v;b=[b,0][b<0];r=[b,r][b<r]
- print(r)
+[[b:=0,print(max(b:=max((b+int(v),0))for v in A.split()))]for A in[*open(0)][2::2]]
